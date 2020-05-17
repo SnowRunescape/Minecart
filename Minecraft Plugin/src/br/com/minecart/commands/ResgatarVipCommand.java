ed8ff290f9ec4793beffbf18934c23647118ae14
SnowRunescape <SnowRunescape@snowdev.com.br>
2020-05-17 10:47:24 -0400

    fix bugs

--- Minecraft Plugin/src/br/com/minecart/commands/ResgatarVipCommand.java
+++ Minecraft Plugin/src/br/com/minecart/commands/ResgatarVipCommand.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -13,18 +14,31 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import br.com.minecart.MineCart;
+import br.com.minecart.storage.LOGStorage;
 import br.com.minecart.utilities.HttpRequest;
 import br.com.minecart.utilities.Messaging;
 
 public class ResgatarVipCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		Player player = (Player) sender;
+		
+		try {
+			if(!MineCart.getDB().checkConnection()) throw new Exception("Error connection database!");
+		} catch(Exception e){
+			player.sendMessage(Messaging.format("error.internal-error", true));
+			
+			return true;
+		}
+		
 		Map<String, String> params = new LinkedHashMap<String, String>();
-		params.put("username", sender.getName());
+		params.put("username", player.getName());
+		
+		Boolean getAnyoneProdct = false;
 		
 		String products = HttpRequest.UrlJsonRequest(MineCart.instance.MineCartAPI + "/getProducts", params);
 		
 		if(products == null || products.isEmpty()){
-			sender.sendMessage(Messaging.format("error.nothing-products", true));
+			player.sendMessage(Messaging.format("error.nothing-products", true));
 			
 			return true;
 		}
@@ -44,18 +58,34 @@ public class ResgatarVipCommand implements CommandExecutor {
 					Integer quantity = productObj.get("quantity").getAsInt();
 					Integer duration = productObj.get("duration").getAsInt();
 					
-					for(int i = 1; i <= quantity; i++) MineCart.instance.keysManage.newKey(sender.getName(), group, duration);
+					for(int i = 1; i <= quantity; i++){
+						if(MineCart.instance.keysManage.newKey(player.getName(), group, duration)){
+							if(!getAnyoneProdct) getAnyoneProdct = true;
+						} else {
+							String msg = MineCart.instance.ResourceMessage.getString("error.redeem-vip");
+							
+							msg = msg.replace("{key.group}", group);
+							msg = msg.replace("{key.duration}", String.valueOf(quantity));
+							
+							player.sendMessage(Messaging.format(msg, false));
+							
+							LOGStorage.resgatarVIP("[ERROR] Ocorreu um erro ao dar o VIP ( "+ group +" ) com duração de ( " + duration + " ) DIAS.");
+						}
+					}
 				}
 				
-				return true;
+				if(getAnyoneProdct){
+					player.sendMessage(Messaging.format("success.redeem-vip", true));
+					
+					return true;
+				}
 			}
 		} catch(Exception e){
 			
 		}
 		
-		sender.sendMessage(Messaging.format("error.nothing-products", true));
+		player.sendMessage(Messaging.format("error.nothing-products", true));
 		
 		return false;
 	}
-
 }
---
+++ Minecraft Plugin/src/br/com/minecart/storage/LOGStorage.java
@@ -0,0 +1,21 @@
+package br.com.minecart.storage;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class LOGStorage {
+	public static void resgatarVIP(String a){
+		try {
+			FileWriter buffWrite = new FileWriter("plugins/MineCart/ResgatarVIP_error.log", true);
+	    	
+			Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Brazil/East"));
+			
+	    	buffWrite.write("[" + calendar.get(Calendar.DAY_OF_MONTH) + "/" + (calendar.get(Calendar.MONTH) + 1) + "/" + calendar.get(Calendar.YEAR) + " - "+ calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE) + ":" + calendar.get(Calendar.SECOND) + "] " + a + "\n");
+			buffWrite.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
